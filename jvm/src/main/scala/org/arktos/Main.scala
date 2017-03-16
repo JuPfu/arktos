af58848a17b305afbbead3bcb0a2daa551ff6702
jp <juergen.pfundt@gmail.com>
2017-03-17 00:45:37 +0100

    Upgrade to Scala 2.12 and ScalaJS

--- jvm/src/main/scala/org/arktos/Main.scala
+++ jvm/src/main/scala/org/arktos/Main.scala
@@ -42,50 +42,74 @@ object Main extends App {
 
   val fmt = cmdLineArgs.get.fmt
 
+  // use "-f uri" or "-f iri" to select URI or IRI parsing
   val parsedURI = if (fmt == "uri") URIParser(input) else IRIParser(input)
 
+  // inquire about the result of the parsing process
   if (parsedURI.isFailure) {
     System.err.println("Input '" + input + "': " + parsedURI.failed.get)
   } else {
+
+    // this is delivered from the URI or IRI parser
     System.out.println("URI=" + parsedURI)
 
+    // get the URI respectively IRI
     val uri = parsedURI.get
 
+    // show all keys contained in the parsed URI respectively IRI
     System.out.println("Get keys " + uri.keys)
+
+    // iterate over keys
+    uri.keys.foreach(x ⇒ System.out.println("Key = " + x + " value = " + uri.getOrElse(x, "")))
+
+    // show all values contained in the parsed URI respectively IRI
     System.out.println("Get values " + uri.values)
-    val uscheme = uri("scheme")
 
-    System.out.println("authority = " + (uscheme + "::" + uri("authority")))
-    System.out.println("scheme = " + uscheme)
+    // get some values
+    System.out.println("authority = " + (if (uri.contains("autority")) uri("authority") else ""))
+    // easier
+    System.out.println("authority = " + uri.getOrElse("autority", ""))
+    // check for key
     System.out.println("Contains 'scheme' is " + uri.contains("scheme"))
+    // get a value with definite fallback
     System.out.println("Get value for 'scheme' = " + uri.getOrElse("scheme", "https"))
-    System.out.println("Parameter to array = " + uri.toParArray)
+    if ( uri.contains("scheme")) {
+      // update "scheme"
+      System.out.println("Update scheme = " + (uri.updated("scheme", "ftp")))
+      // update in a different way
+      System.out.println("Update scheme via '-' and '+' and retrieve value for scheme = " + ((uri - "scheme" + "scheme" → "mailto"))("scheme"))
+      // update in a different way -- last value succeeds
+      System.out.println("Update scheme via '-' and '+' = " + ((uri - "scheme" + "scheme" → "mailto" + "scheme" → "gopher"))("scheme"))
+      // drop key and value
+      val uriWithoutScheme = uri - "scheme"
+      System.out.println("Drop key and value = " + uriWithoutScheme)
+      // add key and value
+      System.out.println("Add key and value = " + (uriWithoutScheme + "scheme" → "ssh"))
+    }
 
-    val prot = uri("protocol")
-    System.out.println("protocol = " + prot)
-    System.out.println("Parameter array(1) = " + (uri.toParArray(1)._2))
-    System.out.println("Drop key and value '-' = " + (uri - "scheme"))
-    System.out.println("Add key and value '+' = " + (uri + ("scheme" → "https")))
-    System.out.println("Concatenation = " + (uri + ("jp" → List(("a", "b")))))
-
-    val params = uri.getOrElse("params", List(("", ""))).asInstanceOf[List[(String, String)]]
+    // get params
+    val params = uri.getOrElse("params", List(("p", "12345"))).asInstanceOf[List[(String, String)]]
+    // print sorted params
     System.out.println("List of sorted Params =" + params.sorted)
-
+    // add tuple at head of params List
+    System.out.println("Add  tuple at head of param list = " + (("u", "$20") :: params))
+    // update params
     System.out.println("Update params = " + uri.updated("params", List(("a", "1"), ("z", "24"))))
 
-    val x = (uri - "params" - "raw_params" + ("params" → List(("x", "u"), ("y", "v"))))("params")
-    System.out.println("params new = " + x.asInstanceOf[List[(String, String)]])
+    val p = (uri - "params" - "raw_params" + ("params" → List(("x", "u"), ("y", "v"))))("params")
+    System.out.println("params new = " + p.asInstanceOf[List[(String, String)]])
 
     // set up a new URI
     val encoder = new URIEncoder
 
-    val uri1: URIType = URI.get +
+    val uri1 = URI.get + // get empty URI
       "scheme" → "https" +
       "path" → "/github.com/JuPfu/arktos/graphs/traffic" +
       "params" → List(("user", "jp"), ("code", encoder.encode("TestA3ßÜ §1")))
 
     System.out.println("uri1 = " + uri1)
 
+    // build uri string
     val uri_synthesized = URI.build(uri)
 
     System.out.println("URI synthesized = " + uri_synthesized)
