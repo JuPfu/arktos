6b879f8c24d10e89490d0c3d58722d68cd0cd970
jp <juergen.pfundt@gmail.com>
2017-04-24 00:20:44 +0200

    consistent usage of ParamsListType and ParamsMapType

--- shared/src/main/scala/org/arktos/URI.scala
+++ shared/src/main/scala/org/arktos/URI.scala
@@ -47,8 +47,8 @@ object URI {
       m.getOrElse("params", default).asInstanceOf[ParamsListType]
     }
 
-    def getParamsAsMap(default: Map[String, List[Option[String]]] = Map.empty) = {
-      if (m.contains("params")) m("params").asInstanceOf[List[(String, Option[String])]].groupBy(_._1).collect { case (x, ys) ⇒ (x, ys.foldLeft(List.empty: List[Option[String]])((k, v) ⇒ k :+ v._2)) }
+    def getParamsAsMap(default: ParamsMapType = Map.empty) = {
+      if (m.contains("params")) m("params").asInstanceOf[ParamsListType].groupBy(_._1).collect { case (x, ys) ⇒ (x, ys.foldLeft(List.empty: List[Option[String]])((k, v) ⇒ k :+ v._2)) }
       else default
     }
 
@@ -64,7 +64,7 @@ object URI {
   }
 
   implicit class ParamsListExtender(val l: ParamsListType) extends AnyVal {
-    def toParamsMap: Map[String, List[Option[String]]] = {
+    def toParamsMap: ParamsMapType = {
       l.groupBy(_._1).collect {
         case (x, ys) ⇒ (x, ys.foldLeft(List.empty: List[Option[String]])((k, v) ⇒ k :+ v._2))
       }
@@ -72,7 +72,7 @@ object URI {
   }
 
   implicit class ParamsMapExtender(val m: ParamsMapType) extends AnyVal {
-    def toParamsList: List[(String, Option[String])] = {
+    def toParamsList: ParamsListType = {
       @tailrec
       def kFactor(k: String)(l: List[Option[String]], r: ListBuffer[(String, Option[String])]): ListBuffer[(String, Option[String])] = l match {
         case x :: xs ⇒ kFactor(k)(xs, r :+ ((k, x)))
